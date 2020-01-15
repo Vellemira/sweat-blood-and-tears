b956b1c773794330db7b2743ced7bd81fe8a4fee
Vlada Nikolaeva <55459933+Vellemira@users.noreply.github.com>
2020-01-16 01:35:13 +0300

    Add files via upload

---
+++ programs/ex10.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int b, i;
+	double a, c;
+	cin >> a >> b;
+	c = a;
+	if (b < 0)
+	{
+		for (i = -1; i > b; i--) c *= a;
+		cout << 1 / c;
+	}
+	else
+	{
+		if (b > 0)
+		{
+			for (i = 1; i < b; i++)
+				c *= a;
+		}
+		if (b == 0)
+		{
+			c = 1;
+		}
+		cout << c;
+	}
+}
