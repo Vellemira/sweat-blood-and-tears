91943b65adf77f339ed0dfbcaa681418acf0d134
Vlada Nikolaeva <55459933+Vellemira@users.noreply.github.com>
2020-01-16 02:02:27 +0300

    Add files via upload

---
+++ programs/ex13.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	unsigned int n, k;
+	int i = 0;
+	k = 1;
+	cout << "Введите целое число от 0 до 10^15: ";
+	cin >> n;
+	while (k <= n) 
+	{
+		k *= 2;
+		i++;
+	}
+	cout << i;
+	return 0;
+}
