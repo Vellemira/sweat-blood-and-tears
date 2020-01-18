47d09c34fc5ea7736ed2b4621cc833f2ee24494b
Vlada Nikolaeva <55459933+Vellemira@users.noreply.github.com>
2020-01-18 10:40:16 +0300

    ex15

---
+++ programs/task15/ex15.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+using namespace std;
+void sort(int* arr, int i)
+{
+	for (int k = 0; k < i - 1; k++)
+	{
+		for (int j = 0; j < i - k - 1; j++)
+		{
+			if (arr[j] < arr[j + 1])
+			{
+				swap(arr[j], arr[j + 1]);
+			}
+		}
+	}
+
+}
+int main()
+{
+	int l, k = 0;
+	cin >> l;
+	int* arr = new int[l];
+	for (int f = 0; f < l; f++) {
+
+		cin >> arr[f];
+	}
+	for (int i = 1; i <= l; i++)
+	{
+		sort(arr, i);
+		if (i > 5) {
+
+			k++;
+		}
+		for (int p = k; p < i; p++)
+		{
+			cout << arr[p] << " ";
+		}
+		cout << endl;
+	}
+	delete[] arr;
+	return 0;
+}
