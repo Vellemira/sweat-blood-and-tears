cb263f938348f179f1fcbaa87bf33afaf4204ec9
Vlada Nikolaeva <55459933+Vellemira@users.noreply.github.com>
2020-01-18 06:09:44 +0300

    лаба

---
+++ programs/task14/ex14.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int sum, l1, l2, r1, r2, x1, x2;
+	cout << "Введите сумму чисел: " << endl;
+	cin >> sum;
+	cout << "Введите первый диапазон чисел: " << endl;
+	cin >> l1 >> r1;
+	cout << "Введите второй диапазон чисел: " << endl;
+	cin >> l2 >> r2;
+	for (int i = l1; i <= r1; i++)
+	{
+		for (int k = l2; k <= r2; k++)
+		{
+			if (i + k == sum)
+			{
+				cout << "x1 = " << i << endl;
+				cout << "x2 = " << k << endl;
+				return 0;
+			}
+		}
+	}
+	cout << -1;
+}
